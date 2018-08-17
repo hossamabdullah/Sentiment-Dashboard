a80aa8290b0a140f2b9f6f012ad41cd0d41e5b71
hossameldeen <hossamabdalh@gmail.com>
2018-08-17 19:13:19 +0200

    making services

---
+++ src/app/services/Sentiment.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SentimentService {
+    constructor(private http: Http) {}
+
+    performSentiment(key: String) {
+        return this.http.get('http://localhost:3003/sentiment/online?keyword='+key);
+    }
+
+
+    getSentiments(key: String){
+        return this.http.get('http://localhost:3003/historyOfTopics?topic='+key);
+    }
+
+    getTweets(sentimentId: String){
+        return this.http.get('http://localhost:3003/historyOfSentences?topic='+sentimentId);
+    }
+    
+}
